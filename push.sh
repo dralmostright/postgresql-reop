b55ecd0727adeb7f3aa08a5924882cd95f5f6c32
dralmostright <dralmostright@gmail.com>
2024-02-01 14:54:42 +0545

    Final Upload

--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git config --global user.name "dralmostright"
+git config --global user.email "dralmostright@gmail.com"
+git config -l
 git add .
 git commit -m "Final Upload"
 git push origin master
